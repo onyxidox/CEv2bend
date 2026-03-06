9b2ec3d544f75bb2c0d04d5c3cffea7abab9cae4
onyxidox <johnalia7890@gmail.com>
2026-03-07 01:27:07 +0500

    Update server.js

--- my-next-app/server.js
+++ my-next-app/server.js
@@ -11,7 +11,14 @@ const syncRoutes = require('./routes/sync');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://fastbsce.vercel.app/",
+    "http://localhost:3000"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use(async (req, res, next) => {
